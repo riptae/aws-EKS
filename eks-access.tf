a8d217f1da4c4bf6d286ea782bc5c7793443be05
yun <riptaggart@naver.com>
2026-02-15 12:32:43 +0900

    [4] access 권한 수정

--- eks-access.tf
+++ eks-access.tf
@@ -5,7 +5,7 @@ resource "aws_eks_access_entry" "admin" {
   depends_on    = [module.eks]
 }
 
-resource "aws_eks_access_policy_association" "adimin" {
+resource "aws_eks_access_policy_association" "admin" {
   cluster_name  = module.eks.cluster_name
   principal_arn = var.admin_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
